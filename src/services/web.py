1e33a72bd9a0d1c77719c6a51311e53d8bee2934
Nikhil Dashore <nikhild1102@gmail.com>
2019-07-01 18:15:51 +0530

    feature: Added endpoint to fetch photos of pending status

--- src/services/web.py
+++ src/services/web.py
@@ -1,5 +1,22 @@
 from app import *
+from models import Photos,PhotoThumbnails,Photo_Status
 
 @app.route("/")
 def index():
     return jsonify(success=True)
+
+@app.route("/photos/pending")
+def pendingPhotos():
+  snapshot = Photos.query.filter_by(status='pending')
+  pendingRecords = []
+
+  if snapshot:
+    for p in snapshot:
+      pendingRecords.append({
+        'uuid':p.uuid,
+        'url':p.url,
+        'status': p.status.value,
+        })
+    return jsonify({'data':pendingRecords})
+  else:
+    return jsonify({'data':[],'message':'No pending records found'})
